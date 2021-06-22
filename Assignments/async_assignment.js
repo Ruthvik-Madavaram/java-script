e679260ffd1d27f361416402cc68e521faee1aff
Ruthvik-Madavaram <ruthvik.madavaram@zemosolabs.com>
2021-06-22 09:46:08 +0530

    js assignment

--- Assignments/async_assignment.js
+++ Assignments/async_assignment.js
@@ -5,31 +5,31 @@
 *
 */
 
-// function getData(uId) {
-//     setTimeout(() => {
-//     console.log("Fetched the data!");
-//     return "skc@gmail.com";
-//     }, 4000);
-//     }
+function getData(uId) {
+    setTimeout(() => {
+    console.log("Fetched the data!");
+    return "skc@gmail.com";
+    }, 4000);
+    }
     
-//     console.log("start");
-//     var email = getData("skc");
-//     console.log("Email id of the user id is: " + email);
-//     console.log("end");
+    console.log("start");
+    var email = getData("skc");
+    console.log("Email id of the user id is: " + email);
+    console.log("end");
     
-    // How do you solve this problem. How can we wait for till the function execution is completed, so that we can have correct email at line 10?
+    //How do you solve this problem. How can we wait for till the function execution is completed, so that we can have correct email at line 10?
 
-    // solution with callback.
-    // function getData(uId,callback) {
-    //     setTimeout(() => {
-    //     console.log("Fetched the data!");
-    //     callback("skc@gmail.com");
-    //     }, 4000);
-    //     }
+    //solution with callback.
+    function getData(uId,callback) {
+        setTimeout(() => {
+        console.log("Fetched the data!");
+        callback("skc@gmail.com");
+        }, 4000);
+        }
         
-    //     console.log("start");
-    //     var email = getData("skc",email=>console.log("Email id of the user id is: " + email));
-    //     console.log("end");
+        console.log("start");
+        var email = getData("skc",email=>console.log("Email id of the user id is: " + email));
+        console.log("end");
 
     //solution with Promise.
     function getData(uId) {
@@ -41,10 +41,11 @@
         });
         }
         
-        // console.log("start");
-        // getData("skc").then(email=>console.log("Email id of the user id is: " + email));
-        // console.log("end");
-     // solution with async await
+        console.log("start");
+        getData("skc").then(email=>console.log("Email id of the user id is: " + email));
+        console.log("end");
+
+     //solution with async await
         async function getUserEmail(){
             loggedUser = await getData("skc");
             console.log(loggedUser);
--- Assignments/es6.js
+++ Assignments/es6.js
@@ -1,18 +1,18 @@
 // Refactor the following function into a one-liner:
-// const printName = (name) => {
-//                      return “Hi” + name;
-//            }
-//const printName = name => "Hi " + name;
+const printName = (name) => {
+                     return “Hi” + name;
+           }
+const printName = name => "Hi " + name;
 
 // Rewrite the following code using template literals
-// const printBill = (name, bill) => {
-//                      return “Hi “ + name + “, please pay: “ + bill;
-//            }
+const printBill = (name, bill) => {
+                     return “Hi “ + name + “, please pay: “ + bill;
+           }
 
-// const printBill = (name,bill) => {
-//     return `Hi ${name}, please pay: ${bill}`;
-// }
-// console.log(printBill("Ruthvik",1500));
+const printBill = (name,bill) => {
+    return `Hi ${name}, please pay: ${bill}`;
+}
+console.log(printBill("Ruthvik",1500));
 
 // Modify the following code such that the object properties are destructured and logged.
 const person = {name:"Noam Chomsky",age:92};
